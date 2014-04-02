6b76c3cadf2c031f5744fe39f09d04a5544cf5c5
Dan Setterquist <dansetterquist@gmail.com>
2014-04-02 14:25:30 +0200

    Made processes work.

--- digenv.c
+++ digenv.c
@@ -4,36 +4,69 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void runCommand(int*, int, char*);
+void createPipe(int*);
 
 int main()
 {
-  int fd[2];
-  int return_value;
-  return_value = pipe(fd);
-  if(return_value == -1) {
-    fprintf(stderr, "Unable to create pipe");
-    exit(1);
+  int five = 55;
+  int pipe_fd1[2];
+  int pipe_fd2[2];
+  int pipe_fd3[2];
+  pid_t pid;
+
+  createPipe(pipe_fd1);
+  createPipe(pipe_fd2);
+  /* createPipe(pipe_fd3); */
+
+  pid = fork();
+  if(pid == 0) {
+    dup2(pipe_fd1[0], 0);
+    dup2(pipe_fd2[1], 1);
+    close(pipe_fd1[0]);
+    close(pipe_fd1[1]);
+    close(pipe_fd2[0]);
+    close(pipe_fd2[1]);
+    execlp("cat", "cat", (char*) 0);
+    /*int number = 0;
+    read(STDIN_FILENO, &number, sizeof(number));
+    number += 3;
+    write(STDOUT_FILENO, &number, sizeof(number));
+    exit(0); */
   }
 
-  runCommand(fd, 0, "sort");
-  runCommand(fd, 1, "printenv");
+  pid = fork();
+  if(pid == 0) {
+    dup2(pipe_fd2[0], 0);
+    close(pipe_fd1[0]);
+    close(pipe_fd1[1]);
+    close(pipe_fd2[0]);
+    close(pipe_fd2[1]);
+    /* dup2(pipe_fd3[1], 1); */
+    execlp("cat", "cat", (char*) 0);
+    /*int number = 0;
+    read(STDIN_FILENO, &number, sizeof(number));
+    number += 7;
+    write(STDOUT_FILENO, &number, sizeof(number));
+    exit(0);*/
+  }
+
+  /* int number = 0; */
+  write(pipe_fd1[1], &five, sizeof(five));
+  close(pipe_fd1[0]);
+  close(pipe_fd1[1]);
+  close(pipe_fd2[0]);
+  close(pipe_fd2[1]);
+  /* read(pipe_fd3[0], &number, sizeof(number));
+     fprintf(stderr, "winning: %d\n", number); */
+
   exit(0);
 }
 
-void runCommand(int* fd, int index, char* command) {
-  pid_t pid;
-  pid = fork();
-  if(pid == 0) {
-    dup2(fd[index], index);
-    close(fd[0]);
-    close(fd[1]);
-    execlp(command, command, (char*) 0);
-    /* If execlp succeeds it does not return, so if we get it failed. */
-    fprintf(stderr, "Unable to start command %s", command);
-    exit(1);
-  } else if(pid == -1) {
-    fprintf(stderr, "Unable to fork");
+void createPipe(int* pipe_fd) {
+  int return_value;
+  return_value = pipe(pipe_fd);
+  if(return_value == -1) {
+    fprintf(stderr, "Unable to create pipe");
     exit(1);
   }
 }
