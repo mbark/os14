bbdabb198af64064dfe053ffbbce47fe0e45b382
Dan Setterquist <dansetterquist@gmail.com>
2014-04-02 14:35:38 +0200

    Run printenv | sort | cat.

--- digenv.c
+++ digenv.c
@@ -8,56 +8,43 @@ void createPipe(int*);
 
 int main()
 {
-  int five = 55;
   int pipe_fd1[2];
   int pipe_fd2[2];
-  int pipe_fd3[2];
   pid_t pid;
 
   createPipe(pipe_fd1);
   createPipe(pipe_fd2);
-  /* createPipe(pipe_fd3); */
 
   pid = fork();
   if(pid == 0) {
     dup2(pipe_fd1[0], 0);
     dup2(pipe_fd2[1], 1);
+    
     close(pipe_fd1[0]);
     close(pipe_fd1[1]);
     close(pipe_fd2[0]);
     close(pipe_fd2[1]);
-    execlp("cat", "cat", (char*) 0);
-    /*int number = 0;
-    read(STDIN_FILENO, &number, sizeof(number));
-    number += 3;
-    write(STDOUT_FILENO, &number, sizeof(number));
-    exit(0); */
+    execlp("sort", "sort", (char*) 0);
   }
 
   pid = fork();
   if(pid == 0) {
     dup2(pipe_fd2[0], 0);
+    
     close(pipe_fd1[0]);
     close(pipe_fd1[1]);
     close(pipe_fd2[0]);
     close(pipe_fd2[1]);
-    /* dup2(pipe_fd3[1], 1); */
     execlp("cat", "cat", (char*) 0);
-    /*int number = 0;
-    read(STDIN_FILENO, &number, sizeof(number));
-    number += 7;
-    write(STDOUT_FILENO, &number, sizeof(number));
-    exit(0);*/
   }
 
   /* int number = 0; */
-  write(pipe_fd1[1], &five, sizeof(five));
+  dup2(pipe_fd1[1], 1);
   close(pipe_fd1[0]);
   close(pipe_fd1[1]);
   close(pipe_fd2[0]);
   close(pipe_fd2[1]);
-  /* read(pipe_fd3[0], &number, sizeof(number));
-     fprintf(stderr, "winning: %d\n", number); */
+  execlp("printenv", "printenv", (char*) 0);
 
   exit(0);
 }
