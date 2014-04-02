426d9236f832f5d0a9f600b9697cefefff5e6c12
Dan Setterquist <dansetterquist@gmail.com>
2014-04-02 15:00:18 +0200

    Added grep stage.

--- digenv.c
+++ digenv.c
@@ -10,25 +10,36 @@ void runCommand(int, int, char*);
 
 int pipe_fd1[2];
 int pipe_fd2[2];
+int pipe_fd3[2];
 
-int main()
+int main(int argc, char *argv[])
 {
   pid_t pid;
 
   createPipe(pipe_fd1);
   createPipe(pipe_fd2);
+  createPipe(pipe_fd3);
 
   pid = fork();
   if(pid == 0) {
     runCommand(0, pipe_fd1[1], "printenv");
   }
 
-  pid = fork();
-  if(pid == 0) {
-    runCommand(pipe_fd1[0], pipe_fd2[1], "sort");
+  if(argc > 1) {
+    pid = fork();
+    if(pid == 0) {
+      runCommand(pipe_fd1[0], pipe_fd2[1], "sort");
+    }
+  } else {
+    dup2(pipe_fd1[0], pipe_fd2[0]);
   }
 
-  runCommand(pipe_fd2[0], 1, "less");
+  pid = fork();
+  if(pid == 0) {
+    runCommand(pipe_fd2[0], pipe_fd3[1], "sort");
+  }
+
+  runCommand(pipe_fd3[0], 1, "less");
   exit(0);
 }
 
@@ -47,7 +58,6 @@ void closePipes() {
   close(pipe_fd2[0]);
   close(pipe_fd2[1]);
 }
-
 
 void runCommand(int in, int out, char* command) {
   dup2(in, 0);
