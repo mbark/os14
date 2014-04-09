2b0e6e21efe5d196bcb0a22924d5eedbd2aae161
Dan Setterquist <dansetterquist@gmail.com>
2014-04-09 14:20:08 +0200

    Cleaned up a bit.

--- digenv.c
+++ digenv.c
@@ -9,6 +9,8 @@ void createPipe(int*);
 void closePipes();
 void runCommand(int, int, char*);
 void runCommandArgs(int, int, char*, char**);
+void redirectStd(int, int);
+void runProcess(int, int, char*, char**);
 
 int pipe_fd1[2];
 int pipe_fd2[2];
@@ -17,34 +19,37 @@ int pipe_fd3[2];
 int main(int argc, char *argv[])
 {
   pid_t pid;
+  char* pager;
+  char* args[1];
 
   createPipe(pipe_fd1);
   createPipe(pipe_fd2);
   createPipe(pipe_fd3);
 
-  pid = fork();
-  if(pid == 0) {
-    runCommand(0, pipe_fd1[1], "printenv");
-  }
+  args[0] = "printenv";
+  runProcess(0, pipe_fd1[1], "printenv", args);
 
   if(argc > 1) {
-    pid = fork();
-    if(pid == 0) {
-      argv[0] = "grep";
-      runCommandArgs(pipe_fd1[0], pipe_fd2[1], "grep", argv);
-    }
+    argv[0] = "grep";
+    runProcess(pipe_fd1[0], pipe_fd2[1], "grep", argv);
   } else {
     dup2(pipe_fd1[0], pipe_fd2[0]);
   }
 
-  pid = fork();
-  if(pid == 0) {
-    runCommand(pipe_fd2[0], pipe_fd3[1], "sort");
-  }
+  args[0] = "sort";
+  runProcess(pipe_fd2[0], pipe_fd3[1], "sort", args);
 
   pid = fork();
   if(pid == 0) {
-    runCommand(pipe_fd3[0], 1, "less");
+    pager = getenv("PAGER");
+    redirectStd(pipe_fd3[0], 1);
+    if(pager != NULL) {
+      execlp(pager, pager, (char*) 0);
+    }
+    execlp("less", "less", (char*) 0);
+    execlp("more", "more", (char*) 0);
+    fprintf(stderr, "Unable to find a pager");
+    exit(1);
   }
 
   closePipes();
@@ -78,9 +83,20 @@ void runCommand(int in, int out, char* command) {
 }
 
 void runCommandArgs(int in, int out, char* command, char** args) {
+  redirectStd(in, out);
+  execvp(command, args);
+}
+
+void redirectStd(int in, int out) {
   dup2(in, 0);
   dup2(out, 1);
 
   closePipes();
-  execvp(command, args);
+}
+
+void runProcess(int in, int out, char* command, char** args) {
+  pid_t pid = fork();
+  if(pid == 0) {
+    runCommandArgs(in, out, command, args);
+  }
 }
