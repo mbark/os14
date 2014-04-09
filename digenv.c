aad75d54d7a7371c4a6af470bddf35c72da4309c
Dan Setterquist <dansetterquist@gmail.com>
2014-04-09 15:39:34 +0200

    Made safe. No bugs.

--- digenv.c
+++ digenv.c
@@ -12,6 +12,8 @@ void runCommandArgs(int, int, char*, char**);
 void redirectStd(int, int);
 pid_t runProcess(int, int*, char*, char**);
 void waitForChild();
+void dup2Safe(int, int);
+void closeSafe(int);
 
 int pipe_fd1[2];
 int pipe_fd2[2];
@@ -28,7 +30,7 @@ int main(int argc, char *argv[])
   args[0] = "printenv";
   runProcess(0, pipe_fd1, "printenv", args);
   waitForChild();
-  close(pipe_fd1[1]);
+  closeSafe(pipe_fd1[1]);
 
   createPipe(pipe_fd2);
 
@@ -36,9 +38,9 @@ int main(int argc, char *argv[])
     argv[0] = "grep";
     runProcess(pipe_fd1[0], pipe_fd2, "grep", argv);
     waitForChild();
-    close(pipe_fd2[1]);
+    closeSafe(pipe_fd2[1]);
   } else {
-    dup2(pipe_fd1[0], pipe_fd2[0]);
+    dup2Safe(pipe_fd1[0], pipe_fd2[0]);
   }
 
   createPipe(pipe_fd3);
@@ -46,12 +48,12 @@ int main(int argc, char *argv[])
   args[0] = "sort";
   runProcess(pipe_fd2[0], pipe_fd3, "sort", args);
   waitForChild();
-  close(pipe_fd3[1]);
+  closeSafe(pipe_fd3[1]);
 
   pid = fork();
   if(pid == 0) {
     pager = getenv("PAGER");
-    dup2(pipe_fd3[0], 0);
+    dup2Safe(pipe_fd3[0], 0);
     if(pager != NULL) {
       execlp(pager, pager, (char*) 0);
     }
@@ -62,9 +64,9 @@ int main(int argc, char *argv[])
   }
 
   waitForChild();
-  close(pipe_fd1[0]);
-  close(pipe_fd2[0]);
-  close(pipe_fd3[0]);
+  closeSafe(pipe_fd1[0]);
+  closeSafe(pipe_fd2[0]);
+  closeSafe(pipe_fd3[0]);
   
   exit(0);
 }
@@ -81,12 +83,14 @@ void createPipe(int* pipe_fd) {
 pid_t runProcess(int in, int* pipe_out, char* command, char** args) {
   pid_t pid = fork();
   if(pid == 0) {
-    dup2(in, 0);
-    dup2(pipe_out[1], 1);
-    close(in);
-    close(pipe_out[0]);
-    close(pipe_out[1]);
+    dup2Safe(in, 0);
+    dup2Safe(pipe_out[1], 1);
+    closeSafe(in);
+    closeSafe(pipe_out[0]);
+    closeSafe(pipe_out[1]);
     execvp(command, args);
+    fprintf(stderr, "Unable to execute command %s", command);
+    exit(1);
   }
 
   return pid;
@@ -94,9 +98,30 @@ pid_t runProcess(int in, int* pipe_out, char* command, char** args) {
 
 void waitForChild() {
   int status;
-  wait(&status);
+  pid_t pid = wait(&status);
+  if(pid == -1) {
+    fprintf(stderr, "Unable to wait for child");
+    exit(1);
+  }
+  
   if(!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
     fprintf(stderr, "Child terminated abnormally\n");
+    exit(1);
+  }
+}
+
+void dup2Safe(int oldfd, int newfd) {
+  int return_value = dup2(oldfd, newfd);
+  if(return_value == -1) {
+    fprintf(stderr, "Unable to execute dup2(%d, %d)", oldfd, newfd);
+    exit(1);
+  }
+}
+
+void closeSafe(int fd) {
+  int return_value = close(fd);
+  if(return_value == -1) {
+    fprintf(stderr, "Unable to execute close(%d)", fd);
     exit(1);
   }
 }
