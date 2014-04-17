472263f4a7d45a3d70b4ffe1974db41610d325e6
Dan Setterquist <dansetterquist@gmail.com>
2014-04-17 14:49:04 +0200

    It works perfectly.

--- lab2/minishell.c
+++ lab2/minishell.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH (70)
 #define MAX_NUM_ARGS (5)
@@ -11,26 +12,26 @@
 void readCommand(char*, int);
 void parseCommand(char*, char**, char**);
 void cd(char*);
-void getDirectory(char*[]);
 void quit();
-void runCommand(char*);
+void runCommandForeground(char*, char*[]);
 
 int main(int argc, char* argv[]) {
   char buffer[MAX_COMMAND_LENGTH];
   char* command;
-  char* args[MAX_NUM_ARGS];
+  char* args[MAX_NUM_ARGS + 1];
   
-  printf("minishell>");
-  readCommand(buffer, MAX_COMMAND_LENGTH);
-  parseCommand(buffer, &command, args);
-  if(strcmp(command, "exit") == 0) {
-    quit();
-  } else if(strcmp(command, "cd") == 0) {
-    cd("");
-  } else {
-    runCommand(command);
+  while(1) {
+    printf("minishell>");
+    readCommand(buffer, MAX_COMMAND_LENGTH);
+    parseCommand(buffer, &command, args);
+    if(strcmp(command, "exit") == 0) {
+      quit();
+    } else if(strcmp(command, "cd") == 0) {
+      cd(args[1]);
+    } else {
+      runCommandForeground(command, args);
+    }
   }
-  
   
   exit(1);
 }
@@ -47,26 +48,35 @@ void readCommand(char* buffer, int max_size) {
 void parseCommand(char* buffer, char** command, char** args) {
   int i;
   *command = strtok(buffer, " ");
-  for(i = 0; i < MAX_NUM_ARGS; i++) {
+  args[0] = *command;
+  for(i = 1; i < MAX_NUM_ARGS + 1; i++) {
     args[i] = strtok(NULL, " ");
   }
 }
 
-void cd(char* path) {
-  printf("cd\n");
-}
+void cd(char* directory) {
+  if(directory == NULL) {
+    directory = getenv("HOME");
+  }
 
-void getDirectory(char* args[]) {
-
+  if(chdir(directory) == -1) {
+    chdir(getenv("HOME"));
+  }
 }
 
 void quit() {
-  printf("quit\n");
   exit(0);
 }
 
-void runCommand(char* command) {
-  /*execlp(buffer, buffer, (char*) 0);
-  fprintf(stderr, "Unable to start command: %s\n", buffer);
-  exit(1);*/
+void runCommandForeground(char* command, char* args[]) {
+  pid_t pid = fork();
+  if(pid == 0) {
+    execvp(command, args);
+    fprintf(stderr, "Unable to start command: %s\n", command);
+    exit(1);
+  } else if(pid > 0) {
+    wait(NULL);
+  } else {
+
+  }
 }
